5b30c44a66e809f18a80e863b8fbe70f84399306
gergovari <work@varigergo.hu>
2023-08-07 06:47:24 +0200

    refactor: mupdf

--- lib/pdf/extractor.dart
+++ lib/pdf/extractor.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdf/pdf.dart';
-import 'package:pdfx/pdfx.dart' as pdfx;
+import 'package:jni/jni.dart';
+import 'package:lazyext/src/third_party/com/artifex/mupdf/fitz/_package.dart'
+    as mupdf;
+import 'package:path_provider/path_provider.dart';
 
 typedef ExerciseCollection = (String, List<Exercise>);
 
 class Exercise {
-  Rect bounds;
+  mupdf.Rect bounds;
   ImageProvider? image;
 
   Exercise({required this.bounds, this.image});
@@ -20,71 +22,85 @@ class Exercise {
 class ExerciseExtractor {
   RegExp exerciseRegex = RegExp(r"^[1-9]\d*\.");
 
-  Future<ImageProvider?> _pdfRectToImage(
-      File file, int pageIndex, Rect rect) async {
-    pdfx.PdfDocument document = await pdfx.PdfDocument.openFile(file.path);
-    pdfx.PdfPage page = await document.getPage(pageIndex + 1);
-    pdfx.PdfPageImage? pageImage = await page.render(
-        width: page.width, height: page.height, cropRect: rect);
-    page.close();
-    document.close();
-    if (pageImage != null) {
-      return MemoryImage(pageImage.bytes);
-    } else {
-      return null;
-    }
+  Future<ImageProvider> _pageRectToImage(
+      mupdf.Page page, mupdf.Rect rect) async {
+    mupdf.Pixmap pixmap =
+        mupdf.Pixmap.ctor4(mupdf.ColorSpace.DeviceRGB, rect, true);
+    mupdf.DrawDevice device = mupdf.DrawDevice.ctor2(pixmap);
+    page.run(device, mupdf.Matrix.Identity(), mupdf.Cookie());
+    String path = (await getTemporaryDirectory()).path;
+    pixmap.saveAsPNG("$path/output.png".toJString());
+    return MemoryImage(await File("$path/output.png").readAsBytes());
   }
 
-  Future<List<Exercise>> _extractExercises(
-      PdfDocument document, File file) async {
+  List<mupdf.StructuredText_TextLine> _getLinesOnPage(mupdf.Page page) {
+    List<mupdf.StructuredText_TextLine> lines = [];
+    mupdf.StructuredText text =
+        page.toStructuredText("preserve-whitespaces".toJString());
+    JArray<mupdf.StructuredText_TextBlock> blocks = text.getBlocks();
+    for (int j = 0; j < blocks.length; j++) {
+      mupdf.StructuredText_TextBlock block = blocks[j];
+      for (int i = 0; i < block.lines.length; i++) {
+        lines.add(block.lines[i]);
+      }
+    }
+    return lines;
+  }
+
+  String _charsToText(JArray<mupdf.StructuredText_TextChar> chars) {
+    String text = "";
+    for (int i = 0; i < chars.length; i++) {
+      text += String.fromCharCode(chars[i].c);
+    }
+    return text;
+  }
+
+  Future<List<Exercise>> _extractExercises(mupdf.Document document) async {
     List<Exercise> exercises = [];
-    for (int i = 0; i < document.pages.count; i++) {
-      List<TextLine> lines =
-          PdfTextExtractor(document).extractTextLines(startPageIndex: i);
+    for (int i = 0; i < document.countPages(i); i++) {
+      mupdf.Page page = document.loadPage(i, i);
+      List<mupdf.StructuredText_TextLine> lines = _getLinesOnPage(page);
       Exercise? prev;
-      TextLine? lastLine;
-      for (TextLine line in lines) {
-        if (exerciseRegex.hasMatch(line.text)) {
+      mupdf.Rect? lastBounds;
+      for (final line in lines) {
+        String text = _charsToText(line.chars);
+        if (exerciseRegex.hasMatch(text)) {
+          print(
+              "$text: ${line.bbox.x0}, ${line.bbox.y0}, ${line.bbox.x1}, ${line.bbox.y1}");
           if (prev == null) {
-            prev = Exercise(bounds: line.bounds);
+            prev = Exercise(bounds: line.bbox);
           } else {
-            prev.bounds = Rect.fromLTRB(0, prev.bounds.top,
-                document.pages[i].size.width, line.bounds.top);
-            ImageProvider? image = await _pdfRectToImage(file, i, prev.bounds);
-            if (image != null) {
-              prev.image = image;
-            }
+            prev.bounds = mupdf.Rect.ctor1(
+                0, prev.bounds.y0, page.getBounds().x1, line.bbox.y0);
+            prev.image = await _pageRectToImage(page, prev.bounds);
             exercises.add(prev.copyWith());
-            prev = Exercise(bounds: line.bounds);
+            prev = Exercise(bounds: line.bbox);
           }
         }
-        lastLine = line;
+        lastBounds = line.bbox;
       }
       Exercise? last = prev;
-      if (last != null && lastLine != null) {
-        last.bounds = Rect.fromLTRB(0, last.bounds.top,
-            document.pages[i].size.width, lastLine.bounds.bottom);
-        ImageProvider? image = await _pdfRectToImage(file, i, last.bounds);
-        if (image != null) {
-          last.image = image;
-        }
+      if (last != null && lastBounds != null) {
+        last.bounds = mupdf.Rect.ctor1(
+            0, last.bounds.y0, page.getBounds().x1, lastBounds.y1);
+        last.image = await _pageRectToImage(page, last.bounds);
         exercises.add(last);
       }
     }
     return exercises;
   }
 
-  String? _getVisualTitle(PdfDocument document) {
-    //throw UnimplementedError();
-    return null;
+  String? _getDocumentTitle(mupdf.Document document) {
+    return document
+        .getMetaData("info:Title".toJString())
+        .toDartString(deleteOriginal: true);
   }
 
   Future<ExerciseCollection> getExerciseCollection(File file) async {
-    PdfDocument document = PdfDocument(inputBytes: await file.readAsBytes());
-
-    return (
-      _getVisualTitle(document) ?? document.documentInformation.title,
-      await _extractExercises(document, file)
-    );
+    mupdf.Document document =
+        mupdf.Document.openDocument(file.path.toJString());
+    String title = _getDocumentTitle(document) ?? "PLACEHOLDER";
+    List<Exercise> exercises = await _extractExercises(document);
+    return (title, exercises);
   }
 }
---
+++ lib/src/third_party/_init.dart
@@ -0,0 +1,7 @@
+import "dart:ffi" as ffi;
+import "package:jni/internal_helpers_for_jnigen.dart";
+
+// Auto-generated initialization code.
+
+final ffi.Pointer<T> Function<T extends ffi.NativeType>(String sym) jniLookup =
+    ProtectedJniExtensions.initGeneratedLibrary("mupdf");
